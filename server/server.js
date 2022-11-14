9723ca5786b680188c8c55b46b6e116295f856fa
druckmax <max_sommerfeld@web.de>
2022-11-14 15:33:21 +0100

    Initializes backend

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config({ path: "./.env" });
+import app from "./app.js";
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log("DB connection successul"));
+
+const port = process.env.PORT || 6969;
+const server = app.listen(port, () => console.log("Listening on port " + port));
