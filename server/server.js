bed3d0f2ddeaa69735b93309edec37d45283ce80
druckmax <max_sommerfeld@web.de>
2022-11-14 15:43:44 +0100

    Adds separate username to .env

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import app from "./app.js";
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
-);
+).replace("<USERNAME>", process.env.DATABASE_USERNAME);
 
 mongoose.connect(DB).then(() => console.log("DB connection successul"));
 
