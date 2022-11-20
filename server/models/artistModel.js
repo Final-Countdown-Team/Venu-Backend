5bd4326c69d0e6ddf9b82b5a267316cd96ca7fc3
Hammed Waheed <worldwid01@gmail.com>
2022-11-20 15:53:32 +0100

    fixed models

--- server/models/artistModel.js
+++ server/models/artistModel.js
@@ -6,185 +6,184 @@ import crypto from "crypto";
 import { hashingPassword } from "../utils/hashingPassword.js";
 
 const artistSchema = mongoose.Schema(
-  {
-    type: {
-      type: String,
-      enum: ["artist"],
-      default: "artist",
-    },
-    name: {
-      type: String,
-      required: [true, "Please provide a name"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Please provide an email"],
-      unique: true,
-      lowercase: true,
-      validate: [validator.isEmail, "Please provide a valid email"],
-    },
-
-  },
-  biography: {
-    type: String,
-    required: [true, "Please provide a biography"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Please provide a price"],
-  },
-  availability: {
-    type: Boolean,
-    default: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minLength: 4,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      // This only works on CREATE and SAVE!!!
-      validator: function (el) {
-        return el === this.password;
-
-    genre: {
-      type: String,
-      required: [true, "Please provide a genre"],
-    },
-    address: {
-      street: {
-        type: String,
-        required: [true, "Please provide a street"],
-      },
-      city: {
-        type: String,
-        required: [true, "Please provide a city"],
-      },
-      zipcode: {
-        type: String,
-        required: [true, "Please provide a zip"],
-
-      },
-    },
-    password: {
-      type: String,
-      required: [true, "Please provide a password"],
-      minLength: 4,
-      select: false,
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, "Please confirm your password"],
-      validate: {
-        // This only works on CREATE and SAVE!!!
-        validator: function (el) {
-          return el === this.password;
-        },
-        message: "Passwords are not the same!",
-      },
-    },
-    profileImage: {
-      type: String,
-      default: "default.jpg",
-    },
-    images: {
-      type: [String],
-      validate: [
-        imageArrayLimit,
-        "The maximum amount of images cannot exceed 3",
-      ],
-    },
-    description: String,
-    mediaLinks: {
-      facebookUrl: {
-        type: String,
-      },
-      twitterUrl: {
-        type: String,
-      },
-      instagramUrl: {
-        type: String,
-      },
-      youtubeUrl: {
-        type: String,
-      },
-    },
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-    active: {
-      type: Boolean,
-      default: true,
-      select: false,
-    },
-  },
-  { timestamps: true }
+	{
+		type: {
+			type: String,
+			enum: ["artist"],
+			default: "artist",
+		},
+		name: {
+			type: String,
+			required: [true, "Please provide a name"],
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, "Please provide an email"],
+			unique: true,
+			lowercase: true,
+			validate: [validator.isEmail, "Please provide a valid email"],
+		},
+		biography: {
+			type: String,
+			required: [true, "Please provide a biography"],
+		},
+		price: {
+			type: Number,
+			required: [true, "Please provide a price"],
+		},
+		availability: {
+			type: Boolean,
+			default: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Please provide a password"],
+			minLength: 4,
+			select: false,
+		},
+		passwordConfirm: {
+			type: String,
+			required: [true, "Please confirm your password"],
+			validate: {
+				// This only works on CREATE and SAVE!!!
+				validator: function (el) {
+					return el === this.password;
+				},
+			},
+		},
+		genre: {
+			type: String,
+			required: [true, "Please provide a genre"],
+		},
+		address: {
+			street: {
+				type: String,
+				required: [true, "Please provide a street"],
+			},
+			city: {
+				type: String,
+				required: [true, "Please provide a city"],
+			},
+			zipcode: {
+				type: String,
+				required: [true, "Please provide a zip"],
+			},
+		},
+		password: {
+			type: String,
+			required: [true, "Please provide a password"],
+			minLength: 4,
+			select: false,
+		},
+		passwordConfirm: {
+			type: String,
+			required: [true, "Please confirm your password"],
+			validate: {
+				// This only works on CREATE and SAVE!!!
+				validator: function (el) {
+					return el === this.password;
+				},
+				message: "Passwords are not the same!",
+			},
+		},
+		profileImage: {
+			type: String,
+			default: "default.jpg",
+		},
+		images: {
+			type: [String],
+			validate: [
+				imageArrayLimit,
+				"The maximum amount of images cannot exceed 3",
+			],
+		},
+		description: String,
+		mediaLinks: {
+			facebookUrl: {
+				type: String,
+			},
+			twitterUrl: {
+				type: String,
+			},
+			instagramUrl: {
+				type: String,
+			},
+			youtubeUrl: {
+				type: String,
+			},
+		},
+		passwordChangedAt: Date,
+		passwordResetToken: String,
+		passwordResetExpires: Date,
+		active: {
+			type: Boolean,
+			default: true,
+			select: false,
+		},
+	},
+	{ timestamps: true }
 );
 
 // Limit length of if image array to <= 3.
 function imageArrayLimit(val) {
-  return val.length <= 3;
+	return val.length <= 3;
 }
 
 artistSchema.pre("save", async function (next) {
-  // Only run this function if password was actually modified
-  if (!this.isModified("password")) return next();
+	// Only run this function if password was actually modified
+	if (!this.isModified("password")) return next();
 
-  // Hash the password with cost of 12
-  await hashingPassword(this);
-  next();
+	// Hash the password with cost of 12
+	await hashingPassword(this);
+	next();
 });
 
 // Update passwordChangedAt property for the user
 artistSchema.pre("save", function (next) {
-  if (!this.isModified("password") || this.isNew) return next();
+	if (!this.isModified("password") || this.isNew) return next();
 
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
+	this.passwordChangedAt = Date.now() - 1000;
+	next();
 });
 
 // INSTANCE METHODS
 // Comparing password when logging in
 artistSchema.methods.correctPassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.Password);
+	return await bcrypt.compare(candidatePassword, this.Password);
 };
 
 artistSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
+	const resetToken = crypto.randomBytes(32).toString("hex");
 
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+	this.passwordResetToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex");
 
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-  return resetToken;
+	return resetToken;
 };
 
 artistSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
+	if (this.passwordChangedAt) {
+		const changedTimestamp = parseInt(
+			this.passwordChangedAt.getTime() / 1000,
+			10
+		);
 
-    return JWTTimestamp < changedTimestamp;
-  }
-  // False means NOT changed
-  return false;
+		return JWTTimestamp < changedTimestamp;
+	}
+	// False means NOT changed
+	return false;
 };
 
 // Query middleware
 artistSchema.pre(/^find/, function (next) {
-  // this points to the current query
-  this.find({ active: { $ne: false } });
-  next();
+	// this points to the current query
+	this.find({ active: { $ne: false } });
+	next();
 });
 
 const Artist = mongoose.model("Artist", artistSchema);
--- server/models/venueModel.js
+++ server/models/venueModel.js
@@ -6,166 +6,166 @@ import crypto from "crypto";
 import { hashingPassword } from "../utils/hashingPassword.js";
 
 const venueSchema = mongoose.Schema(
-  {
-    type: {
-      type: String,
-      enum: ["venue"],
-      default: "venue",
-    },
-    name: {
-      type: String,
-      required: [true, "Please enter a name for your venue"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Please enter your email address"],
-      lowercase: true,
-      validate: [validator.isEmail, "Please provide a valid email address"],
-      unique: true,
-    },
-    address: {
-      street: {
-        type: String,
-        required: [true, "Please enter your street"],
-      },
-      city: {
-        type: String,
-        required: [true, "Please enter your city"],
-      },
-      zipcode: {
-        type: String,
-        required: [true, "Please enter your zipcode"],
-      },
-    },
-    password: {
-      type: String,
-      required: [true, "Please provide a password"],
-      minLength: 8,
-      select: false,
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, "Please confirm your password"],
-      validate: {
-        validator: function (el) {
-          return el === this.password;
-        },
-      },
-    },
-    profileImage: {
-      type: String,
-      default: "default.jpeg",
-    },
-    images: {
-      type: [String],
-      validate: [
-        imageArrayLimit,
-        "The maximum amount of images cannot exceed 3",
-      ],
-    },
-    description: String,
-    website: String,
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-    active: {
-      type: Boolean,
-      default: true,
-      select: false,
-    },
-  },
+	{
+		type: {
+			type: String,
+			enum: ["venue"],
+			default: "venue",
+		},
+		name: {
+			type: String,
+			required: [true, "Please enter a name for your venue"],
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, "Please enter your email address"],
+			lowercase: true,
+			validate: [validator.isEmail, "Please provide a valid email address"],
+			unique: true,
+		},
+		address: {
+			street: {
+				type: String,
+				required: [true, "Please enter your street"],
+			},
+			city: {
+				type: String,
+				required: [true, "Please enter your city"],
+			},
+			zipcode: {
+				type: String,
+				required: [true, "Please enter your zipcode"],
+			},
+		},
+		password: {
+			type: String,
+			required: [true, "Please provide a password"],
+			minLength: 8,
+			select: false,
+		},
+		passwordConfirm: {
+			type: String,
+			required: [true, "Please confirm your password"],
+			validate: {
+				validator: function (el) {
+					return el === this.password;
+				},
+			},
+		},
+		profileImage: {
+			type: String,
+			default: "default.jpeg",
+		},
+		images: {
+			type: [String],
+			validate: [
+				imageArrayLimit,
+				"The maximum amount of images cannot exceed 3",
+			],
+		},
+		description: String,
+		website: String,
+		passwordChangedAt: Date,
+		passwordResetToken: String,
+		passwordResetExpires: Date,
+		active: {
+			type: Boolean,
+			default: true,
+			select: false,
+		},
+		profileImage: {
+			type: String,
+			default: "default.jpeg",
+		},
+		images: {
+			type: [String],
+			validate: [
+				imageArrayLimit,
+				"The maximum amount of images cannot exceed 3",
+			],
+		},
+		price: {
+			type: Number,
+		},
+		availability: {
+			type: Boolean,
+			default: true,
+		},
+		description: String,
+		website: String,
+		passwordChangedAt: Date,
+		passwordResetToken: String,
+		passwordResetExpires: Date,
+		active: {
+			type: Boolean,
+			default: true,
+			select: false,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+		},
+	},
 
-  profileImage: {
-    type: String,
-    default: "default.jpeg",
-  },
-  images: {
-    type: [String],
-    validate: [imageArrayLimit, "The maximum amount of images cannot exceed 3"],
-  },
-  price: {
-    type: Number
-  },
-  availability: {
-    type: Boolean,
-    default: true,
-  },
-  description: String,
-  website: String,
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
-
-  { timestamps: true }
+	{ timestamps: true }
 );
-
 
 // Limit length of if image array to <= 3.
 function imageArrayLimit(val) {
-  return val.length <= 3;
+	return val.length <= 3;
 }
 
 // Hashing password before saving to database
 venueSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  await hashingPassword(this);
-  next();
+	if (!this.isModified("password")) return next();
+	await hashingPassword(this);
+	next();
 });
 // Updating passwordChanged at when password is modified
 venueSchema.pre("save", function (next) {
-  if (!this.isModified("password") || this.isNew) return next();
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
+	if (!this.isModified("password") || this.isNew) return next();
+	this.passwordChangedAt = Date.now() - 1000;
+	next();
 });
 
 // INSTANCE METHODS
 // Comparing password when logging in
 venueSchema.methods.correctPassword = async function (candidatePW) {
-  return await bcrypt.compare(candidatePW, this.password);
+	return await bcrypt.compare(candidatePW, this.password);
 };
 // Generate and hash reset token and save it to current document
 venueSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
+	const resetToken = crypto.randomBytes(32).toString("hex");
 
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+	this.passwordResetToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex");
 
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-  return resetToken;
+	return resetToken;
 };
 
 venueSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
-    return JWTTimestamp < changedTimestamp;
-  }
+	if (this.passwordChangedAt) {
+		const changedTimestamp = parseInt(
+			this.passwordChangedAt.getTime() / 1000,
+			10
+		);
+		return JWTTimestamp < changedTimestamp;
+	}
 
-  return false;
+	return false;
 };
 
 // Query middleware
 // Only show active venues
 venueSchema.pre(/^find/, function (next) {
-  // this points to the current query
-  this.find({ active: { $ne: false } });
-  next();
+	// this points to the current query
+	this.find({ active: { $ne: false } });
+	next();
 });
 
 const Venue = mongoose.model("Venue", venueSchema);
