daf7e9c7b0a2fe671a1944f414fd4a8af727c7ed
druckmax <max_sommerfeld@web.de>
2022-11-22 15:47:53 +0100

    Fixes paths to models

--- data/import-artist-data.js
+++ data/import-artist-data.js
@@ -4,48 +4,46 @@ import mongoose from 'mongoose';
 
 import path from 'path';
 const __dirname = path.resolve();
-dotenv.config({ path: "./env" });
+dotenv.config({ path: './env' });
 
-import Artist from "../models/artistModel.js";
+import Artist from '../server/models/artistModel.js';
 
 const DB = process.env.DATABASE.replace(
-    "<PASSWORD>",   
-    process.env.DATABASE_PASSWORD
-).replace("<USERNAME>", process.env.DATABASE_USERNAME);
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+).replace('<USERNAME>', process.env.DATABASE_USERNAME);
 
-mongoose
-    .connect(DB)
-    .then(() => console.log("DB connection successful!"));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // Read JSON file
 const artists = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/artist.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/data/artist.json`, 'utf-8')
 );
 
 // Import data into DB
 const importData = async () => {
-    try {
-        await Artist.create(artists);
-        console.log("Data successfully loaded!");
-    } catch (err) {
-        console.log(err);
-    }
-    process.exit();
-}
+  try {
+    await Artist.create(artists);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
 
 // Delete all data from collection
 const deleteData = async () => {
-    try {
-        await Artist.deleteMany();
-        console.log("Data successfully deleted!");
-    } catch (err) {
-        console.log(err);
-    }
-    process.exit();
-}
+  try {
+    await Artist.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
 
-if (process.argv[2] === "--import") {
-    importData();
-} else if (process.argv[2] === "--delete") {
-    deleteData();
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
 }
